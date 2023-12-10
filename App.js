90311f03c575c6b32df4c656e27a57ab84b42df6
wanderl8st <151948627+wanderl8st@users.noreply.github.com>
2023-12-10 15:36:42 +0600

    Add files via upload
    вот эта задача

--- App.js
+++ App.js
@@ -1,7 +1,33 @@
-import Layout from "./components/Layout/Layout";
+import React, { useState } from 'react';
+
+import kazakhstanFlagUrl from 'C:\Users\ugvey\project2\React-Car-Rental-Website\my-app\src\images\flag.jpg'
 
 function App() {
-  return <Layout />;
+  const [isRed, setIsRed] = useState(false);
+
+  const handleClick = () => {
+    setIsRed((prevIsRed) => !prevIsRed);
+  };
+
+  return (
+    <div className="App">
+      <header className={`App-header ${isRed ? 'red-text' : 'blue-text'}`}>
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={kazakhstanFlagUrl} className="Flag" alt="flag" />
+        <p>
+          <span onClick={handleClick}>Qazaqstan</span>
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
